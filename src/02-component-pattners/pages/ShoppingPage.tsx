dea39d2c48058105a1f1176baf7a486369146183
Flavio Roman <flavioromanweb@gmail.com>
2023-11-29 21:31:22 -0500

    rama fallida de npm packet

--- src/02-component-pattners/pages/ShoppingPage.tsx
+++ src/02-component-pattners/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components";
 import { products } from "../data/products";
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -14,7 +13,6 @@ export const ShoppingPage = () => {
         <ProductCard 
             key={ product.id } 
             product={ product } 
-            className="bg-dark text-white"
             initialValues={{
                 count: 4,
                 maxCount: 10
@@ -23,15 +21,9 @@ export const ShoppingPage = () => {
             {
                 ({ count, maxCount, isMaxCountReached, reset, increaseBy }) => (
                     <>
-                        <ProductImage className="custom-image"/>
-                        <ProductTitle className=" text-bold"/>
-                        <ProductButtons className="custom-buttons"/>
-                    
-                        <button onClick={ reset }>Reset</button>
-                        <button onClick={ () => increaseBy(-2) }>-2</button>
-                        <button style={{ display: !isMaxCountReached ? 'block' : 'none' }}  onClick={ () => increaseBy(2) }>+2</button>
-                        
-                        <span>{ count } - { maxCount }</span>
+                        <ProductImage />
+                        <ProductTitle />
+                        <ProductButtons/>
                     </>
                 )
             }
